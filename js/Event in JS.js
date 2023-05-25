c6510e2a2cfa472fc3e3f390cf15936d90f2dd08
Nitin1604 <nitinsokhal@gmail.com>
2023-05-25 19:36:59 +0530

    Updated Code for Event in Js

--- js/Event in JS.js
+++ js/Event in JS.js
@@ -10,19 +10,19 @@ document.getElementById("heading_id").addEventListener("click", function(e) {
   event_occured = e.target.id 
   console.log('The heading id is:',event_occured) 
  
-// It will you the distance in the x-axis relative to elememt
+// It will give you the distance in the x-axis relative to elememt
 event_occured = e.offsetX 
 console.log('Distance relative to x-axis relative to element is :',event_occured) 
 
-// It will you the distance in the y-axis relative to elememt
+// It will give you the distance in the y-axis relative to elememt
   event_occured = e.offsetY 
   console.log('Distance relative to y-axis relative to element is :',event_occured) 
  
-// It will you the distance in the x-axis relative to element of browser window
+// It will give you the distance in the x-axis relative to element of browser window
 event_occured = e.clientX 
 console.log('Distance in the x-axis relative to element of browser window is: ',event_occured) 
 
-// It will you the distance in the y-axis relative to element of browser window
+// It will give you the distance in the y-axis relative to element of browser window
   event_occured = e.clientY 
   console.log('Distance in the y-axis relative to element of browser window is: ', event_occured) 
   // location.href = '//codewithharry.com'
